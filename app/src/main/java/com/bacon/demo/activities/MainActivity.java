9a8328ef9b4e87679573da569542a6244cdaaddb
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-11 16:51:57 +0530

    progress bar added

--- app/src/main/java/com/bacon/demo/activities/MainActivity.java
+++ app/src/main/java/com/bacon/demo/activities/MainActivity.java
@@ -1,14 +1,14 @@
 package com.bacon.demo.activities;
 
-import android.content.Intent;
-
-
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import android.view.View;
+import android.widget.ProgressBar;
 
 
 import com.bacon.demo.R;
@@ -20,6 +20,7 @@ import com.bacon.demo.service.LoadFlickerFeed;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
 
 
 
@@ -38,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
 
     MyGridAdapter adapter;
     RecyclerView recyclerView;
+    ProgressBar progressBar;
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -52,7 +54,16 @@ public class MainActivity extends AppCompatActivity {
     private void getImagesForTag(String[] tags)  {
         LoadFlickerFeed task = new LoadFlickerFeed(){
             @Override
+            protected void onPreExecute() {
+                progressBar = (ProgressBar) findViewById(R.id.main_progress);
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
             public void onPostExecute(String result) {
+                progressBar = (ProgressBar) findViewById(R.id.main_progress);
+                progressBar.setVisibility(View.GONE);
                 synchronized(adapter) {
                     try{
                         int startPosition = adapter.getItemCount();
@@ -79,8 +90,54 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         };
+
         task.execute(tags);
 
+    }
+
+    public void addImages(){
+        LoadFlickerFeed task = new LoadFlickerFeed(){
+            @Override
+            protected void onPreExecute() {
+                progressBar = (ProgressBar) findViewById(R.id.main_progress);
+                progressBar.setVisibility(View.VISIBLE);
+
+            }
+
+            @Override
+            public void onPostExecute(String result) {
+                progressBar = (ProgressBar) findViewById(R.id.main_progress);
+                progressBar.setVisibility(View.GONE);
+                synchronized(adapter) {
+                    try{
+                       int startPosition = adapter.getItemCount();
+                        JSONObject jsonObject = new JSONObject(result);
+                        JSONArray jsonArray = (JSONArray) jsonObject.get("results");
+
+                        for (int i=0;i<jsonArray.length();i++) {
+                            ImageModel imageModel = new ImageModel();
+                            int width = 300;
+                            int height = 500;
+                            JSONObject resultObject = jsonArray.getJSONObject(i);
+                            String posterPathUrl = POSTER_BASE_URL +  resultObject.get("poster_path");
+                            imageModel.setUrl(posterPathUrl);
+                            imageModel.setWidth(width);
+                            imageModel.setHeight(height);
+                            adapter.addDrawable(imageModel);
+                            adapter.notifyItemRangeInserted(startPosition, result.length());
+
+                        }
+                    }
+                    catch(JSONException e){
+                        e.printStackTrace();
+                    }
+
+
+                }
+            }
+        };
+
+        task.execute();
     }
 
     @Override
@@ -91,20 +148,31 @@ public class MainActivity extends AppCompatActivity {
         adapter = new MyGridAdapter(this);
         recyclerView = (RecyclerView) findViewById(R.id.grid_view);
 
-        StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+
+        final StaggeredGridLayoutManager staggeredGridLayoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
         recyclerView.setLayoutManager(staggeredGridLayoutManager);
 
+
+        addImages();
+
         scrollListener = new EndlessRecyclerViewScrollListener(staggeredGridLayoutManager){
+            @Override
+            public void onScrolled(RecyclerView view, int dx, int dy) {
+                super.onScrolled(view, dx, dy);
+            }
 
             @Override
             public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
-                String color = colors[((int) (colors.length * Math.random()))];
-                String location = locations[((int) (locations.length * Math.random()))];
-                String term = "settings";
-                String[] tags = new String[]{color,location,term};
-                getImagesForTag(tags);
+                view.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        addImages();
+                    }
+                });
             }
         };
+
+        recyclerView.addOnScrollListener(scrollListener);
 
         recyclerView.setAdapter(adapter);
     }
