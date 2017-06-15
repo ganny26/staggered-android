67276e39c3e53165b5f33342ee3860ec384b79f0
selvaganeshr <selvaganeshrajam@gmail.com>
2017-06-16 01:59:47 +0530

    multi auto text view added

--- app/src/main/java/com/bacon/demo/activities/SearchActivity.java
+++ app/src/main/java/com/bacon/demo/activities/SearchActivity.java
@@ -2,14 +2,54 @@ package com.bacon.demo.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.MultiAutoCompleteTextView;
+
 
 import com.bacon.demo.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+
+    private MultiAutoCompleteTextView  multiAutoCompleteTextView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        String[] androidList={"Andoid","Jelly Bean","Froyo",
+                "Ginger Bread","Eclipse Indigo","Eclipse Juno"};
+
+        multiAutoCompleteTextView = (MultiAutoCompleteTextView) findViewById(R.id.multiAutoCompleteTextView1);
+
+        multiAutoCompleteTextView.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+
+        ArrayAdapter<String> stringArrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1,androidList);
+        multiAutoCompleteTextView.setThreshold(1);
+        multiAutoCompleteTextView.setAdapter(stringArrayAdapter);
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.auto_complete_edit_text, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
