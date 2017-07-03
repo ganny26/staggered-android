d0e783c20e057c7bf8d99b0c4e075a630c628cf4
selvaganeshr <selvaganeshrajam@gmail.com>
2017-07-03 22:27:35 +0530

    infinte listener added

---
+++ app/src/main/java/com/bacon/demo/listener/InfiniteScrollListener.java
@@ -0,0 +1,54 @@
+package com.bacon.demo.listener;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by serajam on 6/28/2017.
+ */
+
+public abstract class InfiniteScrollListener extends RecyclerView.OnScrollListener {
+
+    public static String TAG = "InfiniteScrollListener";
+
+    private int previousTotal = 0; // The total number of items in the dataset after the last load
+    private boolean loading = true; // True if we are still waiting for the last set of data to load.
+    private int visibleThreshold = 1; // The minimum amount of items to have below your current scroll position before loading more.
+    int firstVisibleItem, visibleItemCount, totalItemCount;
+
+
+    private int current_page = 1;
+
+    private LinearLayoutManager mLinearLayoutManager;
+
+
+    @Override
+    public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+        super.onScrollStateChanged(recyclerView, newState);
+    }
+
+    @Override
+    public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        visibleItemCount = recyclerView.getChildCount();
+        totalItemCount = mLinearLayoutManager.getItemCount();
+        firstVisibleItem = mLinearLayoutManager.findFirstVisibleItemPosition();
+
+        if (loading) {
+            if (totalItemCount > previousTotal+1) {
+                loading = false;
+                previousTotal = totalItemCount;
+            }
+        }
+        if (!loading && (totalItemCount - visibleItemCount) <= (firstVisibleItem + visibleThreshold)) {
+            // End has been reached
+            // Do something
+            current_page++;
+            onLoadMore(current_page);
+            loading = true;
+        }
+    }
+
+    public abstract void onLoadMore(int current_page);
+
+}
