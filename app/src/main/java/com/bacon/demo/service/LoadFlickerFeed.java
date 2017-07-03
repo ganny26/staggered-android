723528a48fae18559212d88bbbf9c83d23c43555
selvaganeshr <selvaganeshrajam@gmail.com>
2017-07-03 22:26:39 +0530

    updated layout and main menu

--- app/src/main/java/com/bacon/demo/service/LoadFlickerFeed.java
+++ app/src/main/java/com/bacon/demo/service/LoadFlickerFeed.java
@@ -7,8 +7,10 @@ import android.os.AsyncTask;
 import java.io.*;
 
 
+import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
 
 /**
@@ -20,7 +22,7 @@ public class LoadFlickerFeed extends AsyncTask<Integer,Void,String> {
 
 
     public static final String API_URL = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c79895ea6c454b50e1c7ce72f887f24c";
-
+    //private static final String API_URL = "http://192.168.0.106/recipes/";
 
 
     @Override
@@ -28,13 +30,26 @@ public class LoadFlickerFeed extends AsyncTask<Integer,Void,String> {
 
         int pageNumber = params[0];
         String API_URL_PAGE = API_URL + "&page=" + pageNumber;
+     //   String API_URL_PAGE = API_URL + "?page=" + pageNumber;
         String dataResult = null;
         try{
             OkHttpClient client = new OkHttpClient();
+
             Request request = new Request.Builder()
                     .url(API_URL_PAGE).build();
+//            RequestBody requestBody = new MultipartBody.Builder()
+//                    .setType(MultipartBody.FORM)
+//                    .addFormDataPart("","")
+//                    .build();
+//
+//            Request request = new Request.Builder()
+//                    .url(API_URL_PAGE)
+//                    .method("POST", RequestBody.create(null, new byte[0]))
+//                    .post(requestBody)
+//                    .build();
             Response response = client.newCall(request).execute();
             dataResult =  response.body().string();
+
         }catch(IOException e){
             e.printStackTrace();
         }
