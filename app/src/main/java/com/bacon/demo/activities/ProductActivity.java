f73ac4f56f3beeb09cfbb3b5a655fbf5d5554913
selvaganeshr <selvaganeshrajam@gmail.com>
2017-07-18 01:02:29 +0530

    product activity updated

--- app/src/main/java/com/bacon/demo/activities/ProductActivity.java
+++ app/src/main/java/com/bacon/demo/activities/ProductActivity.java
@@ -1,32 +1,14 @@
 package com.bacon.demo.activities;
 
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
-import android.support.v7.widget.RecyclerView;
-
-import android.widget.ListView;
-
+import android.support.v7.app.AppCompatActivity;
 
 import com.bacon.demo.R;
-import com.bacon.demo.adapter.MyAdapter;
-
-import com.bacon.demo.application.StudentModel;
-
-import java.util.ArrayList;
 
 
 public class ProductActivity extends AppCompatActivity {
 
-    private static final String TAG = "ProductActivity";
 
-
-    private ListView productListView;
-    private MyAdapter myAdapter;
-    private RecyclerView mRecyclerView;
-    private ArrayList<StudentModel> myDataset;
-    private Handler handler;
 
 
     @Override
