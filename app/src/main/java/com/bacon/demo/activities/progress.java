155a936c5d8be9ec91fd1972a32b0fc6e3773f6f
selvaganeshr <selvaganeshrajam@gmail.com>
2017-07-18 01:02:43 +0530

    progress bar level

---
+++ app/src/main/java/com/bacon/demo/activities/progress.java
@@ -0,0 +1,15 @@
+package com.bacon.demo.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.bacon.demo.R;
+
+public class progress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_progress);
+    }
+}
