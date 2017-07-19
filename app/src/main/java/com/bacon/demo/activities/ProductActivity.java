d9b5e29ff0e27636ca00965b8319e4bbdcbdac29
selvaganeshr <selvaganeshrajam@gmail.com>
2017-07-19 23:05:50 +0530

    new layout for visual added

--- app/src/main/java/com/bacon/demo/activities/ProductActivity.java
+++ app/src/main/java/com/bacon/demo/activities/ProductActivity.java
@@ -14,7 +14,7 @@ public class ProductActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product);
+        setContentView(R.layout.carb);
 
 
 
