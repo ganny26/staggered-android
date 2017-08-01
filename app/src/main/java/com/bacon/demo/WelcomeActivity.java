46773077c496f6a81ca78dc022995c18711029e4
selvaganeshr <selvaganeshrajam@gmail.com>
2017-08-01 21:54:12 +0530

    welcome activity

---
+++ app/src/main/java/com/bacon/demo/WelcomeActivity.java
@@ -0,0 +1,15 @@
+package com.bacon.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+    public WelcomeActivity() {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
