ce62e8135dcc58d174a39336bf221a2459597932
selvaganeshr <selvaganeshrajam@gmail.com>
2017-08-20 10:44:47 +0530

    into activity updated

--- app/src/main/java/com/bacon/demo/activities/IntroActivity.java
+++ app/src/main/java/com/bacon/demo/activities/IntroActivity.java
@@ -1,7 +1,10 @@
 package com.bacon.demo.activities;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.bacon.demo.R;
@@ -9,14 +12,18 @@ import com.bacon.demo.utils.FontIconDrawable;
 import com.codemybrainsout.onboarder.AhoyOnboarderActivity;
 import com.codemybrainsout.onboarder.AhoyOnboarderCard;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class IntroActivity extends AhoyOnboarderActivity {
+    private static final String TAG = "IntroActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Log.i(TAG,"device information-----"+getDeviceInformations());
         AhoyOnboarderCard ahoyOnboarderCard1 = new AhoyOnboarderCard("Ingredient search", "Got only tomatoes and potatoes at home? Or allergic to peanuts? Search for multi-cuisine recipes that includes only the available ingredient at home or exclude allergic ingredients.", R.drawable.ic_search_black_24dp);
         AhoyOnboarderCard ahoyOnboarderCard2 = new AhoyOnboarderCard("Semantic search", "Need to cut down some calories? Craving for a dish with your favourite item? No problem. We've got an ear for your needs. Use our auto-complete semantic query.",R.drawable.ic_action_voice_search);
         AhoyOnboarderCard ahoyOnboarderCard3 = new AhoyOnboarderCard("Recipe Reader", "Enjoy cooking while we read out the recipe for you. Just 'pause' if you want more time and we'll cover up for you.",R.drawable.ic_favorite_black_24dp);
@@ -48,5 +55,29 @@ public class IntroActivity extends AhoyOnboarderActivity {
     @Override
     public void onFinishButtonPressed() {
         Toast.makeText(this, "Finish Pressed", Toast.LENGTH_SHORT).show();
+    }
+
+    public JSONObject getDeviceInformations(){
+        JSONObject deviceInfo = new JSONObject();
+        try{
+            deviceInfo.put("device_brand",android.os.Build.BRAND);
+            deviceInfo.put("device_name",android.os.Build.DEVICE);
+            deviceInfo.put("os","android");
+            deviceInfo.put("device_type","mobile");
+            deviceInfo.put("os_version",android.os.Build.VERSION.RELEASE);
+            deviceInfo.put("device_model",android.os.Build.MODEL);
+            deviceInfo.put("device_id",getAndroidDeviceId(this));
+
+        }catch (Exception e){
+            Log.i(TAG,"error while fetching device info");
+        }
+        return deviceInfo;
+
+    }
+
+    public static String getAndroidDeviceId(Context mContext){
+        String androidId = Settings.Secure.getString(mContext.getContentResolver(), Settings.Secure.ANDROID_ID);
+        Log.d(TAG,"android device id"+androidId);
+        return androidId;
     }
 }
--- app/src/main/java/com/bacon/demo/activities/IntroActivity1.java
+++ app/src/main/java/com/bacon/demo/activities/IntroActivity1.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.bacon.demo.R;
+import com.bacon.demo.fragments.SampleSlide;
 import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntro2;
 import com.github.paolorotolo.appintro.AppIntro2Fragment;
 import com.github.paolorotolo.appintro.AppIntroFragment;
+import com.github.paolorotolo.appintro.IndicatorController;
 
 /**
  * Created by serajam on 8/12/2017.
@@ -29,15 +32,18 @@ public class IntroActivity1 extends AppIntro {
 
         // Add your slide fragments here.
         // AppIntro will automatically generate the dots indicator and buttons.
-        addSlide((AppIntro2Fragment.newInstance("Ingredient search", "Got only tomatoes and potatoes at home? Or allergic to peanuts? Search for multi-cuisine recipes that includes only the available ingredient at home or exclude allergic ingredients.",
-                R.drawable.ic_mic_black_24dp, ContextCompat.getColor(this,R.color.intro1))));
+        addSlide(SampleSlide.newInstance(R.layout.intro_page_1));
+        addSlide(SampleSlide.newInstance(R.layout.intro_page_2));
+        addSlide(SampleSlide.newInstance(R.layout.intro_page_3));
+//        addSlide((AppIntro2Fragment.newInstance("Ingredient search", "Got only tomatoes and potatoes at home? Or allergic to peanuts? Search for multi-cuisine recipes that includes only the available ingredient at home or exclude allergic ingredients.",
+//                R.drawable.ic_mic_black_24dp, ContextCompat.getColor(this,R.color.intro1))));
 //
-        addSlide((AppIntro2Fragment.newInstance("Semantic search", "Need to cut down some calories? Craving for a dish with your favourite item? No problem. We've got an ear for your needs. Use our auto-complete semantic query.",
-                R.drawable.ic_favorite_black_24dp, ContextCompat.getColor(this,R.color.intro2))));
+//        addSlide((AppIntro2Fragment.newInstance("Semantic search", "Need to cut down some calories? Craving for a dish with your favourite item? No problem. We've got an ear for your needs. Use our auto-complete semantic query.",
+//                R.drawable.ic_favorite_black_24dp, ContextCompat.getColor(this,R.color.intro2))));
 //
 
-        addSlide((AppIntro2Fragment.newInstance("Recipe Reader", "Enjoy cooking while we read out the recipe for you. Just 'pause' if you want more time and we'll cover up for you",
-                R.drawable.ic_search_black_24dp, ContextCompat.getColor(this,R.color.intro3))));
+//        addSlide((AppIntro2Fragment.newInstance("Recipe Reader", "Enjoy cooking while we read out the recipe for you. Just 'pause' if you want more time and we'll cover up for you",
+//                R.drawable.ic_search_black_24dp, ContextCompat.getColor(this,R.color.intro3))));
 //
 
 
@@ -45,14 +51,19 @@ public class IntroActivity1 extends AppIntro {
 
         // OPTIONAL METHODS
         // Override bar/separator color.
-        setBarColor(Color.parseColor("#3F51B5"));
-        setSeparatorColor(Color.parseColor("#2196F3"));
-        showStatusBar(false);
 
-        // Hide Skip/Done button.
-        showSkipButton(true);
-        setProgressButtonEnabled(true);
-        setDoneText("DONE");
+        showStatusBar(false);
+        showSeparator(false);
+
+        showPagerIndicator(true);
+
+       // setIndicatorColor(R.color.white,R.color.primary_light);
+       // setCustomIndicator();
+
+        showSkipButton(false);
+        setProgressButtonEnabled(false);
+
+
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -62,8 +73,7 @@ public class IntroActivity1 extends AppIntro {
 
         // Turn vibration on and set intensity.
         // NOTE: you will probably need to ask VIBRATE permission in Manifest.
-        setVibrate(true);
-        setVibrateIntensity(30);
+
     }
 
     @Override
