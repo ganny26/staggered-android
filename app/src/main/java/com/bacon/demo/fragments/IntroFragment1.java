615d95feaadac93ff972fccdfb6abfb00bcef4da
selvaganeshr <selvaganeshrajam@gmail.com>
2017-08-20 10:44:37 +0530

    sample slide added

---
+++ app/src/main/java/com/bacon/demo/fragments/IntroFragment1.java
@@ -0,0 +1,31 @@
+package com.bacon.demo.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bacon.demo.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class IntroFragment1 extends Fragment {
+
+
+    public IntroFragment1() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_intro_fragment1, container, false);
+    }
+
+}
