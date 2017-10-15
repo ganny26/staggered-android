749e70fb1da7a56397ad6f18f5eefb2a19d8d6e1
selvaganeshr <selvaganeshrajam@gmail.com>
2017-10-15 18:50:47 +0530

    html text view util created

--- app/src/main/java/com/bacon/demo/utils/HtmlTextView.java
+++ app/src/main/java/com/bacon/demo/utils/HtmlTextView.java
@@ -1,6 +1,6 @@
 package com.bacon.demo.utils;
 /*
- * Copyright (c) 2017 WA2COOK.
+ * Copyright (c) 2017 Selva.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
